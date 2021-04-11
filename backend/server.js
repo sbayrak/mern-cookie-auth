237f39a75b0bda59ab5f159bacc5da52e3452f58
sbayrak <34121748+sbayrak@users.noreply.github.com>
2021-04-11 11:42:17 +0300

    Added custom error middleware

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './utils/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 const app = express();
 dotenv.config();
+import cookieParser from 'cookie-parser';
 
 connectDB();
+app.use(cookieParser());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('hi');
 });
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
